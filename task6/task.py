a580ae15fed6892b1878ae691c8327a7502c0f28
DeniRadionov <108937144+DeniRadionov@users.noreply.github.com>
2024-01-21 20:46:07 +0300

    Update task.py

--- task6/task.py
+++ task6/task.py
@@ -1,77 +1,54 @@
 import json
-
 import numpy as np
 
-def getComparisons(ranks):
-    tables = []
+def list_of_reviews_from_json(str, template):
+  reviews = json.loads(str)
+  reviews_list = [0 for _ in range(len(template))]
 
-    n = len(ranks)
-    m = len(ranks[0])
-    for i in range(m):
-        template = np.zeros((n, n))
-        np.fill_diagonal(template, 0.5)
-        tables.append(template)
+  for i in range(len(reviews)):
+    if type(reviews[i]) is list:
+      for el in reviews[i]:
+        reviews_list[template[el]] = i + 1
+    else:
+      reviews_list[template[reviews[i]]] = i + 1
+  
+  return reviews_list
 
-    for iTable in range(len(tables)):
-        for iRow in range(len(tables[iTable])):
-            rowValue = ranks[iRow][iTable]
-            for iCell in range(len(tables[iTable][iRow])):
-                currTableValue = ranks[iCell][iTable]
-                if currTableValue > rowValue:
-                    tables[iTable][iRow][iCell] = 1
-                if currTableValue < rowValue:
-                    tables[iTable][iRow][iCell] = 0
-                if currTableValue == rowValue:
-                    tables[iTable][iRow][iCell] = 0.5
-    return tables
+def task(*args):
+  experts_count = len(args)
+  template = dict()
+  reviews_count = 0
 
-def calcX(compares):
-    rowsCount = len(compares[0])
-    cellsCount = len(compares[0][0])
-    expCount = len(compares)
-    result = np.zeros((rowsCount, cellsCount))
-    for i in range(0, rowsCount):
-        for j in range(0, cellsCount):
-            mi = 0
-            mp = 0
-            mj = 0
-            for t in range(0, expCount):
-                value = compares[t][i][j]
-                if value == 1:
-                    mi += 1
-                elif value == 0.5:
-                    mp += 1
-                else:
-                    mj += 1
-            resIJ = (1 * (mi / expCount)) + (0.5 * (mp / expCount)) + (0 * (mj / expCount))
-            result[i][j] = resIJ
-    return result
+  for el in json.loads(args[0]):
+    if type(el) is list:
+      for elem in el:
+        template[elem] = reviews_count
+        reviews_count += 1
+    else:
+      template[el] = reviews_count
+      reviews_count += 1
+      
+  matrix = []
+  for reviews_str in args:
+    matrix.append(list_of_reviews_from_json(reviews_str, template))
 
+  x = matrix
+  matrix = []
+  for i in range(reviews_count):
+    sum = 0
+    for j in range(experts_count):
+      sum += x[j][i]
+    matrix.append(sum)
+  matrix = np.matrix(matrix)
 
-def calcGeneralEstimation(xTable, E):
-    n = len(xTable[0])
-    kPrev = np.ones(n) / n
-    kNew = None
-    while True:
-        y = np.matmul(xTable, kPrev)
-        lbd = np.matmul(np.ones(n), y)
-        kNew = (1 / lbd) * y
-        diff = abs(kNew - kPrev)
-        max = diff.max()
-        if max <= E:
-            break
-        else:
-            kPrev = kNew
-    return np.around(kNew, 3)
+  # дисперсия
+  D = np.var(matrix) * reviews_count / (reviews_count - 1)
+  D_max = experts_count ** 2 * (reviews_count ** 3 - reviews_count) / 12 / (reviews_count - 1)
 
-def parseJsonString(str):
-    return json.loads(str)
+  return format(D / D_max, ".2f")
 
-def task(jsonString):
-    table = np.array(parseJsonString(jsonString)).transpose()
+A = '["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]'
+B = '[["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]]'
+C = '["3", ["1", "4"], "2", "6", ["5", "7", "8"], ["9", "10"]]'
 
-    comparisons = getComparisons(table)
-    X = calcX(comparisons)
-    K = calcGeneralEstimation(X, 0.001)
-    result = json.dumps(K.tolist())
-    return result
+print(task(A, B, C))
